0cd1d3ece033875d8e0d17111d9790e0f6d99be1
Peter Ericson <pg.ericson@gmail.com>
2010-09-19 20:52:00 +0200

    Fixed bug Issue #1, marked everything http://github.com/pgericson/Pivotal-Tracker-Easy-Copy/issues#issue/1

--- source/contentscript.js
+++ source/contentscript.js
@@ -1,9 +1,9 @@
 chrome.extension.sendRequest({'action': 'pageStart'}); 
 
 $(document).ready(function(){
-  $('div#content > table#layout div.item').live('click', function(){
-    text = $(this).attr('id').replace(/[a-zA-Z_]+/,"");
-    $(this).fadeOut().fadeIn();
+  $('div#content > table#layout div.item div.notUnderEdit').live('click', function(){
+    text = $(this).attr('id').replace(/.*?(\d+).*/,"$1");
+    $(this).parent().fadeOut(50).fadeIn(50);
     chrome.extension.sendRequest({'text' : text});
   });
 });
